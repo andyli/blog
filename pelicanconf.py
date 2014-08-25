21879f172ee52d38ec8bd63a4431328239f13384
Andy Li <andy@onthewings.net>
2014-08-26 06:20:12 +0800

    Added robots.txt

--- pelicanconf.py
+++ pelicanconf.py
@@ -56,6 +56,7 @@ STATIC_PATHS = [
 
 EXTRA_PATH_METADATA = {
     'files/googleb165fe55002fa457.html': {'path': 'googleb165fe55002fa457.html'},
+    'files/robots.txt': {'path': 'robots.txt'},
 }
 
 THEME = 'theme'
