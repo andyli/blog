29331e47eb98021157640ac313bb443edab006a8
Andy Li <andy@onthewings.net>
2016-08-25 16:45:12 +0800

    use https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://blog.onthewings.net'
+SITEURL = 'https://blog.onthewings.net'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = 'http://feeds.feedburner.com'
